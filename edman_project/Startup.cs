733e9ecb06e2182490c3827aa1f0a9576095ec08
Rencel Vocales <rencelallison@gmail.com>
2018-10-01 22:14:53 +0800

    initialize project

---
+++ edman_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(edman_project.Startup))]
+namespace edman_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
